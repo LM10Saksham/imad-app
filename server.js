f366e665b73dfc34bdfd0e6ea259c178b51e2d45
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-08-15 12:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
- ArticleOne  = {
+ var ArticleOne  = {
     title : 'Article-one',
     heading : 'BIO',
     date : '12 August, 2017',
