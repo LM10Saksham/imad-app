152dff2607fc7781b6dbd99d04d7b842b9387fc2
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-08-27 22:40:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,4 @@
-/*var button = document.getElementById('counter');
-
-
+var button = document.getElementById('counter');
 button.onclick = function(){
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
@@ -15,7 +13,7 @@ button.onclick = function(){
     request.open('GET', 'http://sakshambarcelona.imad.hasura-app.io/counter',true);
     request.send(null);
 };
-*/
+
 /*var nameinput = document.getElementById('name');
 var name = nameinput.value;
 var submit = document.getElementById('submit_btn');
