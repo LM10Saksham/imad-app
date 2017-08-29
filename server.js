ec89eafa1a83ab7328b2d80a62545163b6f851e7
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-08-30 01:35:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
- var article-one ={
+ var ArticleOne ={
     title : 'Article-one',
     heading : 'BIO',
     date : '12 August, 2017',
@@ -62,7 +62,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var articles = ['article-one'];
+var articles = ['ArticleOne'];
 
 app.get('/:articlename', function(req, res){
     var articlename = req.params.articlename;
