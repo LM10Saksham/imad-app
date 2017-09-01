449eb8afb7fff7fd2b2ac13bb8a76e272c8d0e19
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-09-01 22:50:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 var config = {
     user : 'sakshambarcelona',
     database : 'sakshambarcelona',
@@ -65,12 +66,17 @@ var htmlTemplate = `
    return htmlTemplate;
 }
 
+function hash(input, salt){
+ var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/hash/:input', function(req,res){
-    var hashedString = hash (req.params.input);
+    var hashedString = hash (req.params.input, 'this-is-some-random-string');
     res.send(hashedString);
 });
 
