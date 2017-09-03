7de069dc7ada3033dae0736be31235e7eb5c2cdf
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-09-03 16:56:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,8 +50,6 @@ var submit = document.getElementById('submit');
 submit.onclick = function(){
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
- console.log(username);
- console.log(password);
     var request = new XMLHttpRequest();
     request.onreadystatechange = function(){
         if(request.readyState === XMLHttpRequest.DONE){
@@ -61,6 +59,6 @@ submit.onclick = function(){
         }
     };
         request.open('POST', 'http://sakshambarcelona.imad.hasura-app.io/login', true);
-        request.setRequestHeader('Content-Type', 'applicatoion/json');
+        request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({username : username, password : password}));
 };
