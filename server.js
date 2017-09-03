4f3eb0711640e3bc716895e7e5f0e91fd09195ad
LM10Saksham <30689254+LM10Saksham@users.noreply.github.com>
2017-09-03 13:26:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ var htmlTemplate = `
 
 function hash(input, salt){
  var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');   //using the pbkdf2Sync fucntion
-    return hashed.toString('hex');
+    return ['pbkdf2','10000', salt,hashed.toString('hex')].join('$');
 }    // function that performs hashing
 
 app.get('/', function (req, res) {
